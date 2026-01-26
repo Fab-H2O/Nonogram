52f449e4bc70585221723c58eebb4939d85bb68e
glubst <adam.timothee@gmail.com>
2026-01-26 14:59:41 +0100

    modification css
    quelques petites modification pour faire un style plus propre

--- app/template/signInForm.php
+++ app/template/signInForm.php
@@ -1,8 +1,13 @@
-<form action="index.php?action=signIn" method="post" class="form" id="signInForm">  
-    <button type="button" onclick="closeElement('#signInForm')" class="closeButton">✖</button>  
-    <label for="username_signInForm">Choisissez un pseudo : </label> 
-    <input type="text" name="username" id="username_signInForm" autocomplete="off">
-    <label for="password_signInForm">Choisissez un mot de passe : </label> 
-    <input type="password" name="password" id="password_signInForm" autocomplete="off">
-    <input type="submit" value="Valider">
+<form action="index.php?action=signIn" method="post" class="form" id="signInForm"> 
+    <div class="boxHeadLine">    
+        <p>Inscription</p> 
+        <button type="button" onclick="closeElement('#signInForm')" class="closeButton">✖</button> 
+    </div>  
+    <div class="formFields">
+        <label for="username_signInForm">Choisissez un pseudo : </label> 
+        <input type="text" name="username" id="username_signInForm" autocomplete="off">
+        <label for="password_signInForm">Choisissez un mot de passe : </label> 
+        <input type="password" name="password" id="password_signInForm" autocomplete="off">
+        <input type="submit" value="Valider">
+    </div>
 </form>
