2ab1ceccab56816d1e51d21877cd326e3fe553f9
alexgretatravail-afk <alex.greta.travail@gmail.com>
2026-01-27 17:01:25 +0100

    correction et avancement  basic html et css
    nettoyage et re arrangement pour coller a la vision du projet + note sure les classe et id dans le css et calcule pour le génération des grilles de nonograms en note dans le JavaScript

---
+++ Web-appli-legere/js/test.js
@@ -0,0 +1,9 @@
+// compte temporaire pour la 
+// generation des grilles de nonograms 
+// à effacer plus tard 
+// 5/5 = 1 carrées de 25 carrées
+// 10/10 = 4 carrées de 25 carrées donc 100 carrées
+// 20/20 = 16 carrées de 25 carrées chacun donc 400 carrées
+// 30/30 = ????
+// random/éditeur de puzzle = ???? 
+// ???? = calcule a effectuer
